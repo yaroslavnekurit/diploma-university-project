99914598444914c4f888b9798ad43c172d6e58f1
Yaroslav <yaroslav@MacBook-Pro-Maxim.local>
2023-04-27 21:21:30 +0300

    feat: add css style to gallery section

--- src/js/app.js
+++ src/js/app.js
@@ -65,3 +65,39 @@ for (let i = 0; i < translate.length; i++) {
 		// Чтобы на следующем клике, убрать класс уже с этого.
 	})
 }
+
+$('.gallery__slider').slick({
+	dots: true,
+	arrows: false,
+	speed: 300,
+	slidesToShow: 3,
+	slidesToScroll: 3,
+	responsive: [
+		{
+			breakpoint: 1024,
+			settings: {
+				slidesToShow: 3,
+				slidesToScroll: 3,
+				infinite: true,
+				dots: true,
+				arrows: false
+			}
+		},
+		{
+			breakpoint: 600,
+			settings: {
+				slidesToShow: 2,
+				slidesToScroll: 2,
+				arrows: false
+			}
+		},
+		{
+			breakpoint: 480,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				arrows: false
+			}
+		}
+	]
+})
